c2a599f3bd3952ffd8e7225b004fa6fb91a0f14d
Gurinder Chauhan <gurinder@apsonex.com>
2020-10-30 18:10:24 -0400

    wip

--- src/Runtime/Http/Middleware/EnsureOnNakedDomain.php
+++ src/Runtime/Http/Middleware/EnsureOnNakedDomain.php
@@ -10,15 +10,15 @@ class EnsureOnNakedDomain
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  callable  $next
+     * @param \Illuminate\Http\Request $request
+     * @param callable                 $next
      * @return mixed
      */
     public function handle($request, $next)
     {
-        if ('https://'.$request->getHttpHost() === $_ENV['APP_VANITY_URL']) {
-            return $next($request);
-        }
+        //        if ('https://'.$request->getHttpHost() === $_ENV['APP_VANITY_URL']) {
+        //            return $next($request);
+        //        }
 
         if (config('servotizer.redirect_to_root') === true &&
             strpos($request->getHost(), 'www.') === 0) {
@@ -30,8 +30,8 @@ class EnsureOnNakedDomain
         if (config('servotizer.redirect_to_root') === false &&
             strpos($request->getHost(), 'www.') === false) {
             return new RedirectResponse(str_replace(
-                $request->getScheme().'://',
-                $request->getScheme().'://www.',
+                $request->getScheme() . '://',
+                $request->getScheme() . '://www.',
                 $request->fullUrl()
             ), 301);
         }
--- src/Runtime/Http/Middleware/EnsureVanityUrlIsNotIndexed.php
+++ src/Runtime/Http/Middleware/EnsureVanityUrlIsNotIndexed.php
@@ -7,17 +7,17 @@ class EnsureVanityUrlIsNotIndexed
     /**
      * Handle the incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  callable  $next
+     * @param \Illuminate\Http\Request $request
+     * @param callable                 $next
      * @return mixed
      */
     public function handle($request, $next)
     {
         $response = $next($request);
 
-        if ('https://'.$request->getHttpHost() === $_ENV['APP_VANITY_URL']) {
-            $response->headers->set('X-Robots-Tag', 'noindex, nofollow', true);
-        }
+        //        if ('https://'.$request->getHttpHost() === $_ENV['APP_VANITY_URL']) {
+        //            $response->headers->set('X-Robots-Tag', 'noindex, nofollow', true);
+        //        }
 
         return $response;
     }
